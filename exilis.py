504353bd08ce5c74c5d3781d75b875d3e78e193b
Andrii Muzichenko <a.muzichenko@ukr.net>
2019-11-22 21:52:26 +0200

    /

--- exilis.py
+++ exilis.py
@@ -1,2 +1,11 @@
-class my(object):
-    pass
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import keras_applications
+import keras_preprocessing
+
+class StockImage(object):
+    image = img.imread(r'C:\Users\Karolus\Desktop\Python\test\lear\object_0.png')
+
+if __name__ == "__main__":
+    StockImage()
