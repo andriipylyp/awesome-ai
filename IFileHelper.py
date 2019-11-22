242413aad6e7da9e5e89cc4ca64ed33179b0e32f
Andrii Pylypenko <andreypilip@hotmail.com>
2019-11-22 20:48:41 +0100

    interfaces init

---
+++ IFileHelper.py
@@ -0,0 +1,9 @@
+class IFileHelper(object):
+    def __init__(self):
+        pass
+
+    def LoadImage(self, file_name):
+        raise Exception("NotImplementedException")
+
+    def SaveMatrix(self, file_name):
+        raise Exception("NotImplementedException")
---
+++ IProcessUnit.py
@@ -0,0 +1,22 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+class IProcessUnit(object):
+    def __init__(self):
+        self.train_data = []
+        self.model = Sequential()
+        self.model.add(Dense(64, activation='relu', input_dim=10))
+        self.model.add(Dense(10, activation='softmax'))
+        self.model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+    def AddDataToTrain(self, matrix):
+        raise Exception("NotImplementedException")
+
+    def Train(self):
+        raise Exception("NotImplementedException")
+
+    def Evaluate(self, file_name):
+        raise Exception("NotImplementedException")
