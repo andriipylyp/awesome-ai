841eb89b0230964b7822ac5aeebc8ed9c1223a4a
Andrii Pylypenko <andreypilip@hotmail.com>
2019-11-22 20:49:16 +0100

    fix

--- IProcessUnit.py
+++ IProcessUnit.py
@@ -9,7 +9,7 @@ class IProcessUnit(object):
         self.model.add(Dense(64, activation='relu', input_dim=10))
         self.model.add(Dense(10, activation='softmax'))
         self.model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
+              optimizer='sgd',
               metrics=['accuracy'])
 
     def AddDataToTrain(self, matrix):
