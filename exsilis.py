f39156c6e05f9c6bba40bc3d9e819d6450b46344
Andrii Muzichenko <a.muzichenko@ukr.net>
2019-11-23 17:45:36 +0200

    rename folder "preview _image" to "preview_image"

--- exsilis.py
+++ exsilis.py
@@ -4,23 +4,23 @@ import matplotlib.image as img
 import time
 
 class StockImage(object):
-    image = img.imread(r"preview _image/stock/object_stock.png")
+    image = img.imread(r"preview_image/stock/object_stock.png")    
 
-class InputPath(): 
-        timer = (1 / 60)
+class InputPath():   #TODO:Rename
+        lag = (1 / 60)
         object_num = 0 
 
         while True:
-            path = '\"' + 'preview_image/object_' + str(object_num) + '.png' + '\"'
+            path = 'preview_image/object_' + str(object_num) + '.png' 
             object_num += 1 
-            time.sleep(timer)   
+            time.sleep(lag)   
             
-            if object_num > 3:
+            if object_num > 6:  #TODO:Off
                 break
             
             #print(path)            
-            #camera_img = img.imread()
-            #print(camera_img)  
+            camera_img = img.imread(path)
+            print(camera_img) 
 
 if __name__ == "__main__":
     InputPath()
